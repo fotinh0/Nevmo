cbcc75af4f3e334bdc7c2c7a0f672af0cee143ee
Foti Ceci <foti.ceci@gmail.com>
2024-10-27 16:36:05 -0400

    create protected route component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,21 @@ import LandingPage from './components/landingPage/LandingPage';
 import DashboardHome from './components/dashboard/home/DashboardHome';
 import Transactions from './components/dashboard/transactions/Transactions';
 import ManageAccounts from './components/dashboard/accounts/ManageAccounts';
+import ProtectedRoute from './components/dashboard/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />}>
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        >
           <Route index element={<DashboardHome />} />
           <Route path="transactions" element={<Transactions />} />
           <Route path="accounts" element={<ManageAccounts />} />
