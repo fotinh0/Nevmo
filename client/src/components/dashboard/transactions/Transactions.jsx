ec9d6007d78c5b2ff2678d96cdab977ab3befafd
Foti Ceci <foti.ceci@gmail.com>
2024-10-27 17:07:44 -0400

    create transactions UI

--- client/src/components/dashboard/transactions/Transactions.jsx
+++ client/src/components/dashboard/transactions/Transactions.jsx
@@ -1,7 +1,30 @@
+import { useOutletContext } from 'react-router-dom';
+
 const Transactions = () => {
+  const { userData } = useOutletContext();
   return (
     <>
-      <h1>Transactions</h1>
+      <h2 className="text-2xl font-bold mb-4">Transactions</h2>
+      {/* Transactions Section */}
+      <div className="space-y-4">
+        {userData.transactions.map((transaction, index) => (
+          <div
+            key={index}
+            className="bg-white p-4 rounded-lg shadow-md flex justify-between items-center"
+          >
+            <div>
+              <p className="font-medium">{transaction.type}</p>
+              <p className="text-sm text-gray-500">
+                {transaction.from} → {transaction.to}
+              </p>
+              <p className="text-sm text-gray-500">Date: {transaction.date}</p>
+            </div>
+            <p className="text-lg font-semibold text-blue-500">
+              ${transaction.amount}
+            </p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
