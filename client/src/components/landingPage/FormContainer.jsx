972bc879fece67f407498609146c5c260d3f993c
Foti Ceci <foti.ceci@gmail.com>
2024-10-27 16:30:59 -0400

    fix landing page margin

--- client/src/components/landingPage/FormContainer.jsx
+++ client/src/components/landingPage/FormContainer.jsx
@@ -5,7 +5,7 @@ import CustomButton from './CustomButton';
 const FormContainer = ({ showSignUp, setShowSignUp }) => {
   const label = showSignUp ? 'Sign up' : 'Log in';
   return (
-    <div className="flex flex-col items-center mx-auto my-0 max-w-md border border-solid border-[#dbd8d0] rounded p-14">
+    <div className="flex flex-col items-center mx-auto my-16 max-w-md border border-solid border-[#dbd8d0] rounded p-14">
       <h1 className="text-3xl text-sky-blue font-bold italic">nevmo</h1>
       <h2 className="text-2xl my-8">{label}</h2>
 
