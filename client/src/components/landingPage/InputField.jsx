38b108e1a5f710339727ba1618ca853cd00d57d1
Foti Ceci <foti.ceci@gmail.com>
2024-10-27 21:49:54 -0400

    update input fields css

--- client/src/components/landingPage/InputField.jsx
+++ client/src/components/landingPage/InputField.jsx
@@ -1,9 +1,7 @@
 const InputField = ({ name, type, placeholder, className, onChange }) => {
   return (
     <input
-      className={`h-14 py-1.5 px-2.5 border border-solid rounded border-[#888C94] focus-visible:outline-sky-blue focus-visible:placeholder:text-sky-blue ${
-        className || ''
-      }`}
+      className={className}
       name={name}
       type={type}
       placeholder={placeholder}
